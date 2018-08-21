480f7274231a4f0ddc514f77f4f79079487db9cc
Paul Crowley <paulcrowley@google.com>
2018-08-21 15:19:44 -0700

    Generalize hpolyc for nh variant

--- python/hpolyc.py
+++ python/hpolyc.py
@@ -12,45 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import cipher
-import latindance
-import xconstruct
+import hbsh
 import poly1305
-import aes
 
-class HPolyC(cipher.Blockcipher):
-    def __init__(self):
-        self._stream = xconstruct.XConstruct(latindance.ChaCha())
-
-    def variant_name(self):
-        return "{}_{}_{}".format(self.name(),
-            self._stream.variant_name(),
-            self._block.variant_name())
-
-    def _blockciphers(self):
-        a = aes.AES()
-        a.set_keylen(32)
-        return [a]
-
-    def _lookup_block(self, v):
-        for b in self._blockciphers():
-            if b.variant == v:
-                return b
-        raise Exception(f"Unknown block cipher: {v}")
-
-    def variants(self):
-        for bs in self._blockciphers():
-            for s in self._stream.variants():
-                yield {
-                    "cipher": self.name(),
-                    "streamcipher": s,
-                    "blockcipher": bs.variant,
-                    "lengths": {"key": 32}}
-
-    def _setup_variant(self):
-        self._block = self._lookup_block(self.variant['blockcipher'])
-        self._stream.variant = self.variant["streamcipher"]
-
+class HPolyC(hbsh.HBSH):
     def _setup_key(self, key):
         self._stream_key = key
         km = self._stream_xor(b'', b'\0' * 48)
@@ -68,32 +33,5 @@ class HPolyC(cipher.Blockcipher):
         i &= ((1<<128)-1)
         return i.to_bytes(16, byteorder='little')
 
-    def _stream_xor(self, nonce, m):
-        needed = self._stream.lengths()["nonce"]
-        padded_nonce = nonce + b'\1' + b'\0' * (needed - len(nonce) -1)
-        return self._stream.encrypt(m, key=self._stream_key, nonce=padded_nonce)
-
-    def encrypt(self, block, key, tweak):
-        self._setup_key(key)
-        pl, pr = block[:-16], block[-16:]
-        pm = self._block_add(pr, self._hash(tweak, pl))
-        cm = self._block.encrypt(pm, key=self._block_key)
-        cl = self._stream_xor(cm, pl)
-        cr = self._block_add(cm, -self._hash(tweak, cl))
-        return cl + cr
-
-    def decrypt(self, block, key, tweak):
-        self._setup_key(key)
-        cl, cr = block[:-16], block[-16:]
-        cm = self._block_add(cr, self._hash(tweak, cl))
-        pl = self._stream_xor(cm, cl)
-        pm = self._block.decrypt(cm, key=self._block_key)
-        pr = self._block_add(pm, -self._hash(tweak, pl))
-        return pl + pr
-
-    def test_input_lengths(self):
-        v = self.lengths()
-        for tlen in 0, 12, 17:
-            for mlen in 16, 31, 128:
-                for m in "plaintext", "ciphertext":
-                    yield {**v, "tweak": tlen, m: mlen}
+    def _block_subtract(self, block, tosubtract):
+        return self._block_add(block, -tosubtract)
