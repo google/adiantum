c7715d80c78f0c250917f5431df52d8f377aa545
Paul Crowley <paulcrowley@google.com>
2018-09-27 09:08:30 -0700

    New Adiantum hash algorithm

--- python/adiantum.py
+++ python/adiantum.py
@@ -23,20 +23,20 @@ class Adiantum(hbsh.HBSH):
 
     def _setup_key(self, key):
         self._stream_key = key
-        r, self._block_key, self._nh_key = self._setup_key_helper(
-            [16, 32, self._nh.lengths()["key"]])
-        self._polyr = poly1305.read_r(r)
+        self._block_key, rt, rm, self._nh_key = self._setup_key_helper(
+            [32, 16, 16, self._nh.lengths()["key"]])
+        self._polyrt = poly1305.read_r(rt)
+        self._polyrm = poly1305.read_r(rm)
 
     def _hash(self, tweak, msg):
-        tohash = (8*len(tweak)).to_bytes(8, byteorder='little')
-        tohash += (8*len(msg)).to_bytes(8, byteorder='little')
-        tohash += tweak
-        tohash += b'\0' * (-len(tohash) % 16)
+        ht = poly1305.poly1305_h_rbar(self._polyrt,
+            (8*len(msg)).to_bytes(16, byteorder='little') + tweak)
         il = self._nh.lengths()
         msg += b'\0' * (-len(msg) % il['unit'])
-        tohash += b"".join(self._nh.nh(self._nh_key, msg[i:i + il['messagemax']])
-            for i in range(0, len(msg), il['messagemax']))
-        return poly1305.poly1305_h_rbar(self._polyr, tohash)
+        hm = poly1305.poly1305_h_rbar(self._polyrm,
+            b"".join(self._nh.nh(self._nh_key, msg[i:i + il['messagemax']])
+                for i in range(0, len(msg), il['messagemax'])))
+        return ht + hm
 
     def test_input_lengths(self):
         for tlen in 0, 12, 17:
